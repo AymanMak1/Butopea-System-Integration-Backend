466036de29b8a37e5b8028ea43ef0d62dd896f35
Webstain <ayman.makhoukhi@gmail.com>
2022-01-19 23:03:04 +0100

    Coming-to-an-end

--- productsfeed.py
+++ productsfeed.py
@@ -1,22 +1,61 @@
+import sqlite3
+import pandas as pd
 import xml.etree.ElementTree as xml
+
+conn = sqlite3.connect("./data.sqlite")
+cur = conn.cursor()
+query1='SELECT p.product_id as ID,'\
+        'pd.name as title,'\
+        'pd.description,'\
+        '"https://butopea.com/" || p.image as image_link,'\
+        'p.price, m.name as brand '\
+        'FROM product p,'\
+        'product_description pd,'\
+        'manufacturer m '\
+        'WHERE p.product_id = pd.product_id AND '\
+        'm.manufacturer_id = p.manufacturer_id '\
+        'AND status = "1";'\
+        
+
 root = xml.Element("Products")
 
 def GenerateXML(fileName):
- 
-    child = xml.Element("Product")
-    root.append(child)
-    ID = xml.SubElement(child,"ID")
-    Title = xml.SubElement(child,"Title")
-    Description = xml.SubElement(child,"Description")
-    Link = xml.SubElement(child,"Link")
-    Image = xml.SubElement(child,"Image")
-    ImageLink = xml.SubElement(child,"ImageLink ")
-    AdditionalImageLinks = xml.SubElement(child,"AdditionalImageLinks")
-    Price = xml.SubElement(child,"Price")
-    Brand = xml.SubElement(child,"Brand")
-    Condition = xml.SubElement(child,"Condition")
-    ID.text = "196"
+    df = pd.read_sql(query1, conn)
+    for i in range(len(df)) :
+        child = xml.Element("Product")
+        root.append(child)
+        ID = xml.SubElement(child,"ID")
+        ID.text = df.loc[i,"ID"]
+        Title = xml.SubElement(child,"Title")
+        Title.text = df.loc[i,"title"]
+        Description = xml.SubElement(child,"Description")
+        Description.text = df.loc[i,"description"]
+        Link = xml.SubElement(child,"Link")
+        Link.text = 'https://butopea.com/p/' + df.loc[i,"ID"]
+        ImageLink = xml.SubElement(child,"ImageLink")
+        ImageLink.text = df.loc[i,"image_link"]
+        AdditionalImageLinks = xml.SubElement(child,"AdditionalImageLinks")
+        query2 ='SELECT p.product_id as ID,'\
+                 '"https://butopea.com/"|| pi.image AS additional_image_link,'\
+                 'sort_order '\
+                 'FROM product p, product_image pi '\
+                 'WHERE pi.product_id = p.product_id AND '\
+                 'p.product_id=' + str(df.loc[i,"ID"]) + ' AND status = "1" Order by sort_order;'\
 
+        df2 = pd.read_sql(query2, conn)
+        for j in range(len(df2)):
+            AdditionalImageLinks.text = df2.loc[j,"additional_image_link"]
+                
+        Price = xml.SubElement(child,"Price")
+        Price.text = df.loc[i,"price"]
+        Brand = xml.SubElement(child,"Brand")
+        Brand.text = df.loc[i,"brand"]
+        Condition = xml.SubElement(child,"Condition")
+        Condition.text = 'new'
+        
+    
+    
+    conn.close()
     tree = xml.ElementTree(root)
     with open(fileName,"wb") as feed:
         tree.write(feed)
